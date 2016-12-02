59934603cc15d97a93002bb39ea12b7a2f267fa7
Nico Linder <nicolinder@icloud.com>
2016-12-02 02:24:12 +0100

    trying to implement connection to trainfever.net. Failing at the moment because of Terms of Conditions

--- TPFMM/Library.fs
+++ TPFMM/Library.fs
@@ -2,6 +2,10 @@
 
 open FSharp.Data
 open System
+open System.Net
+
+type private Url = Url of String
+type private WebCode = WebCode of String
 
 // Internal logic to provide API functionality
 module private Internal =
@@ -19,7 +23,7 @@ module private Internal =
 
     let loadMods () = loadModsFrom "mods.json"
 
-    let modStatus url =
+    let modStatus (Url url) =
         let mods = loadMods()
         let fold state (m :Mods.InstalledMod) = state || m.Url = url
         let installed = List.fold fold false mods
@@ -28,8 +32,43 @@ module private Internal =
         | false -> NotInstalled
 
     // Functionality
+    let cookieContainer = new CookieContainer()
+
+    let acceptTerms (Url urlString) =
+        let fold inputs input =
+            let fold (n, v) (atr :HtmlAttribute) =
+                match atr.Name() with
+                | "name" -> (atr.Value(), v)
+                | "value" -> (n, atr.Value())
+                | _ -> (n, v)
+            let atrPair = HtmlNode.attributes input |> List.fold fold ("", "")
+            let (name, value) = atrPair
+            printfn "%s-%s" name value
+            atrPair::inputs
+
+        let site = HtmlDocument.Load(urlString)
+        let query = site.CssSelect("#content form input") |> List.fold fold []
+        let action = site.CssSelect("#content form")
+        match action with
+        | [action] ->
+            let action' = action |> HtmlNode.attribute "action"
+         // Send HttpRequest
+            Http.RequestString (action'.Value(), body = FormValues query, httpMethod="POST", cookieContainer=cookieContainer) |> ignore
+        | _ -> failwith ("[Error] Confirmation of Terms failed! - "+urlString)
+
+
+    let nameFromSite (Url urlString) =
+        let source = Http.RequestString (urlString, cookieContainer=cookieContainer)
+        let source' = HtmlDocument.Parse(source)
+        let node = source'.CssSelect("#content header > h1 > a")
+        match node with
+        | [header] -> HtmlNode.innerText header
+        | _ -> failwith "[Error] Unsupported layout of website! - "+urlString
+
     let downloadMod url =
-        printf "%s" "I download!" |> ignore
+        let (Url urlString) = url
+        acceptTerms url
+        printfn "%s" (nameFromSite url)
 
     let list () =
         printfn "%s" "Installed mods:"
@@ -40,9 +79,10 @@ module private Internal =
         |> List.iter printMod
 
     let install url =
+        let (Url urlString) = url
         match modStatus url with
         | Installed ->
-            printfn "A mod with url '%s' is already installed." url
+            printfn "[Error] A mod with url '%s' is already installed." urlString
         | NotInstalled ->
             downloadMod url
 
@@ -52,5 +92,8 @@ module private Internal =
 // API
 type TPFMM =
     static member List = Internal.list
-    static member InstallAll urls = Internal.installAll urls
-    static member Install url = Internal.install url
+    static member Install url = Internal.install (Url url)
+    static member InstallAll urls =
+        Array.toList urls
+        |> List.map (fun url -> Url url)
+        |> Internal.installAll
--- TPFMM/Program.fs
+++ TPFMM/Program.fs
@@ -5,8 +5,8 @@ open System
 let main argv = 
     let args = Array.toList argv
     match args with
-    | [] -> ConsoleApp.execCommand "help" []
-    | command::args -> ConsoleApp.execCommand command args
+    | [] -> ConsoleApp.execCommand "help" [||]
+    | command::args -> ConsoleApp.execCommand command (Array.ofList args)
     0
 
 [<EntryPoint>]
@@ -14,7 +14,7 @@ let main argv =
 let test args =
     match System.Diagnostics.Debugger.IsAttached with
     | true ->
-        main [| "install" ; "https://www.transportfever.net/filebase/index.php/Entry/2322-v200-in-verschiedenen-Versionen/" |] |> ignore
+        main [| "install" ; "https://www.transportfever.net/filebase/index.php/Entry/2322-v200-in-verschiedenen-Versionen/" ; "https://www.transportfever.net/filebase/index.php/Entry/2448-DMA-Modern-Buses/" |] |> ignore
         Console.ReadKey() |> ignore
         0
     | false ->
