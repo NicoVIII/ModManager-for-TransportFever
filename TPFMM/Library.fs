ee22e2dd2c7b1b3d88b636a8f13e5bceb29443d6
Nico Linder <nicolinder@icloud.com>
2016-12-08 23:15:03 +0100

    remove this stupid "i give the archive a own name instead of the name it has!" stuff to avoid umlaut errors

--- TPFMM/Library.fs
+++ TPFMM/Library.fs
@@ -33,8 +33,7 @@ module private Internal =
     let saveBytes {name = name; version = version; url = url; bytes = bytes; title = title} =
         // Replace invalid characters
         let name' = invalidFilePathChars |> List.fold (fun (name :string) c -> name.Replace(c, "_")) name
-        let extension::_ = title.Split '.' |> List.ofArray |> List.rev
-        let path = ("tmp/"+name'+"-"+version+"."+extension)
+        let path = ("tmp/"+title)
         directoryFromFile path
         |> Directory.CreateDirectory |> ignore
         File.WriteAllBytes(path, bytes)
@@ -97,49 +96,6 @@ module private Internal =
         >> map (tee (ApiHelper.convertExtractInfo >> extractStartedEvent.Trigger))
         >> bind (performExtract settings)
         >> map (tee (ApiHelper.convertMod >> extractEndedEvent.Trigger))
-
-    (*let prepareExtractZip (settings :Settings) modInfoDownloaded =
-        let {name = name; version = version; url = url; archivePath = zipPath} = modInfoDownloaded
-        use file = ZipFile.Open(zipPath, ZipArchiveMode.Read)
-        let fold list (entry :ZipArchiveEntry) =
-            let name = entry.FullName.TrimEnd '/'
-            let name' = (name.Split '/').[0]
-            if List.forall (fun el -> not (el = name')) list then
-                name'::list
-            else
-                list
-        let entries = file.Entries |> Seq.toList |> List.fold fold []
-        file.Dispose ()
-        match entries with
-        | [folder] -> 
-            Ok {modDownloadedInfo=modInfoDownloaded; extractPath = settings.tpfModPath; folder = folder}
-        | list ->
-            if list |> List.exists (fun el -> el = "mod.lua") then
-                Ok {modDownloadedInfo=modInfoDownloaded; extractPath = settings.tpfModPath+"/"+name; folder = name}
-            else 
-                Error [ModInvalid]
-
-    let performExtractZip (settings :Settings) {modDownloadedInfo = {name = name; version = version; url = url; archivePath = zipPath}; folder = folder; extractPath = extractPath} =
-        try
-            ZipFile.ExtractToDirectory(zipPath, extractPath) |> ignore
-            if settings.deleteArchives then File.Delete(zipPath)
-            Ok {name = name; websiteVersion = version; url = url; folder = folder}
-        with
-        | :? System.IO.IOException -> Error [ExtractionFailed]
-
-    let extractZip (settings :Settings) (extractStartedEvent :Event<_>) (extractEndedEvent :Event<_>) =
-        prepareExtractZip settings
-        >> map (tee (ApiHelper.convertExtractInfo >> extractStartedEvent.Trigger))
-        >> bind (performExtractZip settings)
-        >> map (tee (ApiHelper.convertMod >> extractEndedEvent.Trigger))*)
-
-    (*let extract (settings :Settings) (extractStartedEvent :Event<_>) (extractEndedEvent :Event<_>) (modInfoDownloaded :ModDownloadedInfo) =
-        if modInfoDownloaded.archivePath.EndsWith ".zip" then
-            extractZip settings extractStartedEvent extractEndedEvent modInfoDownloaded
-        else if modInfoDownloaded.archivePath.EndsWith ".rar" then
-            extractRar settings extractStartedEvent extractEndedEvent modInfoDownloaded
-        else
-            Error [NotSupportedFormat "?"]*)
 
     let install (settings :Settings) (downloadStartedEvent :Event<_>) (downloadEndedEvent :Event<_>) (extractStartedEvent :Event<_>) (extractEndedEvent :Event<_>) =
         checkModInstalled
