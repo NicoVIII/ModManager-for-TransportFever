8a3c26fe202a755bd49e21c4f23f9575e68134cf
Nico Linder <nicolinder@icloud.com>
2017-01-06 12:00:14 +0100

    first experiments with adding GUI

---
+++ TPFMM_Gui/ModList.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Xwt;
+
+namespace TPFMM_Gui {
+	public class ModList : Table {
+		public ModList() {
+		}
+	}
+}
---
+++ TPFMM_Gui/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xwt;
+using Xwt.Drawing;
+
+namespace TPFMM_Gui {
+	class Program {
+		[STAThread]
+		static void Main(string[] args) {
+			string title = "TPF-ModManager v0.1-alpha.5";
+			// Init Gui
+			PlatformID id = Environment.OSVersion.Platform;
+			switch (id) {
+				case PlatformID.Win32NT:
+					Application.Initialize(ToolkitType.Wpf);
+					break;
+				//case PlatformID.MacOSX:
+				//case PlatformID.Unix:
+				default:
+					Application.Initialize(ToolkitType.Gtk);
+					break;
+			}
+			var mainWindow = new Window() {
+				Title = title,
+
+				Decorated = true,
+				Width = 800,
+				Height = 600
+			};
+
+			Box container = new VBox();
+			container.Margin = new WidgetSpacing(5, 5, 5, 5);
+			container.BackgroundColor = Colors.Black;
+			mainWindow.Content = container;
+
+			mainWindow.MainMenu = new MainMenu();
+
+			// Start Application
+			mainWindow.Show();
+			Application.Run();
+			mainWindow.Dispose();
+		}
+	}
+}
